0b9c52de592e26d09ee3938f8a07172415da6215
Babin Bohora <bohorababin1@gmail.com>
2022-07-09 02:28:30 +0900

    list added and automatic fetch done

--- popup.js
+++ popup.js
@@ -1,9 +1,26 @@
+// LIST FETCHER
+( () => {
+    // send message
+    chrome.runtime.sendMessage({ action: "fetch_text" });
+  }
+)();
+
+
+const addLine = (message) => {
+  const list = document.getElementById("popup-list")
+  const listItem = document.createElement("li");
+  listItem.innerHTML = message;
+  list.appendChild(listItem);
+};
+
+// Listening to MESSAGE
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  const {action, message} = request;
-  console.log("HELLO");
-  if (action === "display_text") {
-    console.log("(POPUP) message:", message);
+  const {action, message, list} = request;
+  if (action === "display_text" && message) {
+    addLine(message);
+  } else if (action === "display_list" && list) {
+    list.forEach(text => addLine(text));
   }
 });
 
-console.log("HELER");
+// BUTTON TO CLEAR STORAGE

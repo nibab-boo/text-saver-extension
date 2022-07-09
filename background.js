04191b2d9b9e1335eddb20ace4f460ea09e323d7
Babin Bohora <bohorababin1@gmail.com>
2022-07-09 20:27:09 +0900

    remove injected script from background

--- background.js
+++ background.js
@@ -1,16 +1,13 @@
-// INJECT FOREGROUND.js to access user html
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {  
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ["./foreground.js"]
-        })
-            .then(() => {
-                console.log("INJECTED THE FOREGROUND SCRIPT.");
-            })
-            .catch(err => console.log(err));
-    }
-});
+// CUSTOM POPUP LOAD
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     console.log("check URL: ", (/^https:\/\/www.google.com/.test(tab.url)));
+
+//     if (/^https:\/\/www.google.com/.test(tab.url)) {  
+//         chrome.action.setPopup({ popup: "./popup.html"})
+//     } else {
+//         chrome.action.setPopup({ popup: "" })
+//     }
+// });
 
 
 // Send Text To POPOUT.js
